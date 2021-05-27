8acbb7e3cfd7bb69ed1f863f811e284a33f0b9a1
Artem <arpavlovscky@yandex.ru>
2021-05-28 00:44:13 +0300

    finished

--- main.js
+++ main.js
@@ -2,12 +2,3 @@ function openMenu() {
     document.getElementById('sidebar').classList.toggle('active');
 }
 
-function about() {
-    var about = document.querySelector('.about');
-    about.href = "http://vk.com";
-}
-
-function contacts () {
-    alert ('Sorry! But this link is not working!')
-}
-
